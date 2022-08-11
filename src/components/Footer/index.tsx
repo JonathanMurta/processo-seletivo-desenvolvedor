08f63ef95a57c2cdbd5e6d66fc2b74fe3ffffbc1
Jonathan Murta <44501547+JonathanMurta@users.noreply.github.com>
2022-08-10 23:56:12 -0300

    Create: Criação do site

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import style from './style.module.css'
+
+export default function Footer()
+{
+    return (
+        <footer>
+            <div className={style["a-footer"]}>
+                <span>2022 - Todos os direitos reservados</span>
+            </div>
+        </footer>
+    )
+}
---
+++ src/types/users.ts
@@ -0,0 +1,9 @@
+export interface IUsers
+{
+    email: string,
+    firstname: string,
+    lastname: string,
+    username: string,
+    website: string,
+    uuid: string
+}
